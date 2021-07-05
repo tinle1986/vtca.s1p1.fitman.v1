caf839fa9554c4a533a76de843aaff3cbc733e47
tinle1986 <letrungtin1986@gmail.com>
2021-07-05 09:03:09 +0700

    Initial Commit

---
+++ FitMan/FitManApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitManApp.swift
+//  FitMan
+//
+//  Created by Blackswan on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct FitManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
